f9062fe749f4b24f9aeff34a2104c8a8a65a7b5a
RaymundoSantorski <rayma9829@gmail.com>
2020-12-29 12:29:55 -0600

    Primer commit

---
+++ public/js/socket-chat.js
@@ -0,0 +1,53 @@
+var socket = io();
+
+var params = new URLSearchParams(window.location.search);
+if (!params.has('nombre') || !params.has('sala')) {
+    window.location = 'index.html';
+    throw new Error('El nombre y sala con necesario');
+}
+
+var usuario = {
+    nombre: params.get('nombre'),
+    sala: params.get('sala')
+};
+
+socket.on('connect', function() {
+    console.log('Conectado al servidor');
+
+    socket.emit('entrarChat', usuario, function(response) {
+        console.log(response);
+    });
+});
+
+// Escuchar cuando un usuario se desconecta
+socket.on('disconnect', function() {
+
+    console.log('Perdimos conexión con el servidor');
+
+});
+
+
+// Enviar información
+// socket.emit('crearMensaje', {
+//     usuario: 'Fernando',
+//     mensaje: 'Hola Mundo'
+// }, function(resp) {
+//     console.log('respuesta server: ', resp);
+// });
+
+// Escuchar información
+socket.on('crearMensaje', function(mensaje) {
+
+    console.log('Servidor:', mensaje);
+
+});
+
+// Escuchar cambios de usuarios
+socket.on('listaPersonas', function(personas) {
+    console.log(personas);
+});
+
+// Mensajes privados
+socket.on('mensajePrivado', function(data) {
+    console.log('Mensaje privado:', data);
+});
